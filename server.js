fb2b40f6fc5c9a1e47d5395c4c8253ad174de9a9
Nastasha Leach <nastasha.leach@outlook.com>
2021-07-24 10:59:15 -0500

    Added Partners and Promotions Router

--- server.js
+++ server.js
@@ -2,35 +2,18 @@
 const express = require("express");
 const morgan = require("morgan");    //require Morgan
 const campsiteRouter = require("./routes/campsiteRouter");
+const promotionsRouter = require("./routes/promotionsRouter");
+const partnersRouter = require("./routes/partnersRouter");
 const hostname = "localhost";
 const port = 3000;
 const app = express();
 app.use(morgan("dev"));   //Configures morgan to log using the development version. Will log request information
 app.use(express.json()); //This will handle parsing json data to javascript properties of the request object
 
+//CREATE CAMPSITES ROUTER
 app.use("/campsites", campsiteRouter);
-
-//EXPRESS METHODS
-
-//We will work these during the workshop assignment this weekend. She said delete, but I will just comment out
-// app.get("/campsites/:campsiteId", (req, res) => {
-//     res.end(`Will send details of the campsite ${req.params.campsiteId} to you.`);
-// });
-
-// app.post("/campsites/:campsiteId", (req, res) => {
-//     res.statusCode = 403;
-//     res.end(`Post operations not supported on /campsites/${req.params.campsiteId}.`);
-// });
-
-// app.put("/campsites/:campsiteId", (req, res) => {
-//     res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-//     res.end(`Will update the campsite ${req.body.name} with description: ${req.body.description}.`);
-// });
-
-// app.delete("/campsites/:campsiteId", (req, res) => {
-//     res.end(`Deleting campsite ${req.params.campsiteId}.`);
-// });
-
+app.use("/promotions", promotionsRouter);
+app.use("/partners", partnersRouter);
 
 //SERVE FILES
 app.use(express.static(__dirname + "/public"));  //dirname is a special variable in node. it has 2 underscores this is all that is needed to server static files from public folder
